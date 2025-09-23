8dd7e7ce1e3c5099f970920e998277ee07cf40db
Shruthi-Patil <shruthipatil053@gmail.com>
2025-09-23 08:57:58 +0200

    minor change

--- zoomin/disaggregation_utils.py
+++ zoomin/disaggregation_utils.py
@@ -58,9 +58,9 @@ def solve_proxy_equation(equation: str, target_resolution: str):
     for var_name in var_list:
         proxy_data = get_proxy_data(var_name, target_resolution)
 
-        # If there is no variance in data, we cannot normailize it. So everything is just set to 0
+        # If there is no variance in data, we cannot normailize it. So everything is just set to 1
         if (len(proxy_data) > 1) & (len(proxy_data["value"].unique()) == 1):
-            proxy_data["value"] = 0
+            proxy_data["value"] = 1
         else:
             proxy_data["value"] = (
                 proxy_data["value"] / proxy_data["value"].max()
